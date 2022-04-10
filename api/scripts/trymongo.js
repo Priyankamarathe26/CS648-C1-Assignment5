815ea0acae807468ba427ba12b3ea2751b07ef3c
Priyankamarathe26 <41960982+Priyankamarathe26@users.noreply.github.com>
2022-04-10 16:53:49 -0700

    Hidden password
    Hide password

--- api/scripts/trymongo.js
+++ api/scripts/trymongo.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const url = process.env.DB_URL || 'mongodb+srv://Priyanka26:AAUPM8095m*@nodedatabase.zmwgk.mongodb.net/myFirstDatabase?retryWrites=true';
+const url = process.env.DB_URL || 'mongodb+srv://Priyanka26:<password>@nodedatabase.zmwgk.mongodb.net/myFirstDatabase?retryWrites=true';
 
 
 function testWithCallbacks(callback) {
